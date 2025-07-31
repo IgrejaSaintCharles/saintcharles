d53a7bf1f2665f4e4b1c18640e53a8069129d2b1
Saint Charles <henriquehtessmer@outlook.com>
2025-07-31 03:17:00 +0000

    tentativa pixel

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,12 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
+// 👉 Importa a função do seu pixel.ts
+import { initFacebookPixel } from './pixel.ts';
+
+// 👉 Inicializa o pixel ANTES da renderização
+initFacebookPixel();
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
