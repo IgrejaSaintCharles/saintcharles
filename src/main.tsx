235b87524ba01ba09c55899e9e62e926bd552170
Saint Charles <henriquehtessmer@outlook.com>
2025-07-31 03:36:42 +0000

    remocao script

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,6 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
-// 👉 Importa a função do seu pixel.ts
-import { initFacebookPixel } from './pixel.ts';
-
-// 👉 Inicializa o pixel ANTES da renderização
-initFacebookPixel()
-console.log('Pixel do Facebook carregado');
-;
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
